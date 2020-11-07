d03571a0732e5568092e1390188865f689c93128
michalm4139 <michal41.39@gmail.com>
2020-11-07 18:20:37 +0100

    CLR added

--- server/src/docs/boolean.ts
+++ server/src/docs/boolean.ts
@@ -2,5 +2,25 @@ import { CompletionItem, CompletionItemKind, MarkedString, MarkupContent, Markup
 
 
 export const boolean : CompletionItem[]  = [
+{
+	label: "CLR",
+	kind: CompletionItemKind.Keyword,
+	detail: "Clear Carry flag",
+	documentation: {
+		kind: MarkupKind.Markdown,
+		value: 
+`
+CLR clears (sets to 0) all the bit(s) of the indicated register.
+If the register is a bit (including the carry bit), only the specified bit is affected.
+Clearing the Accumulator sets the Accumulator's value to 0.
+\nSyntax:\n
 
+\`\`\`asm8051
+ADDC A, operand
+\`\`\`
+
+__Additional info__
+`.trim(),
+	},
+},
 ]
