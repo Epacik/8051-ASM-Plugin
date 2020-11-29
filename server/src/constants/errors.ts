e399d6fcfb36611f69dee03a82941c06b5746d10
epacik <epaciatko@gmail.com>
2020-11-29 12:50:02 +0100

    moved error messages to constants

---
+++ server/src/constants/errors.ts
@@ -0,0 +1,8 @@
+export namespace errors {
+	export const missingLabel = "(8051: E0001) Missing label"
+	export const instructionNoWhitespace = "(8051: E0002) Instructions have to start with a whitespace";
+	export const labelWithWhitespace = "(8051: E0003) Labels cannot start with a whitespace"
+	export const missingOprand = "(8051: E0004) Missing operand";
+	export const missingComa = "(8051: E0005) Missing comma";
+	export const wrongOperand = "(8051: E0006) Wrong operand";
+}
--- server/src/diagnostics/errors.ts
+++
@@ -1,9 +0,0 @@
-export namespace errors {
-	export const missingLabel = "(8051E0001) Missing label"
-	export const instructionNoWhitespace = "(8051E0002) Instructions have to start with a whitespace";
-	export const labelWithWhitespace = "(8051E0003) Labels cannot start with a whitespace"
-	export const missingOprand = "(8051E0004) Missing operand";
-	export const missingComa = "(8051E0005) Missing comma";
-	export const wrongOperand = "(8051E0006) Wrong operand";
-	
-}
