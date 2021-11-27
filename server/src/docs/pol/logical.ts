85309988f7701d88aafcca41c89ce2d06aa46a57
Epat <i.am@epat.xyz>
2021-11-27 09:50:01 +0100

    throw everything out of the window

--- server/src/docs/pol/logical.ts
+++
@@ -1,5 +0,0 @@
-import { CompletionItem, CompletionItemKind, MarkedString, MarkupContent, MarkupKind } from 'vscode-languageserver';
-
-export const logical : Map<string,CompletionItem> = new Map([
-	
-]);
--- server/src/docs/pol/pins.ts
+++
@@ -1,5 +0,0 @@
-import { CompletionItem, CompletionItemKind, MarkedString, MarkupContent, MarkupKind } from 'vscode-languageserver';
-
-export const pins : Map<string,CompletionItem> = new Map([
-
-]);
