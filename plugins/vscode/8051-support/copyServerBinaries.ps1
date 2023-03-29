73496441e9b1fa0c401b61d2b024a4bcfeea5ff7
Epat <i.am@epat.xyz>
2023-03-30 01:56:35 +0200

    update copying binaries for debugging client

--- plugins/vscode/8051-support/copyServerBinaries.ps1
+++ plugins/vscode/8051-support/copyServerBinaries.ps1
@@ -7,14 +7,33 @@ try {
 }
 catch {}
 
+New-Item "$PSScriptRoot/out/bin/" -Force -ItemType Directory
 
 if ($IsLinux) {
+    $path = "$PSScriptRoot/../../../server/asm8051_lsp/target/x86_64-unknown-linux-gnu/release/asm8051_lsp";
+
+    if (!(Test-Path $path)){
+        $path = "$PSScriptRoot/../../../server/asm8051_lsp/target/release/asm8051_lsp";
+    }
     
-    Copy-Item "$PSScriptRoot/../../../server/asm8051_lsp/target/x86_64-unknown-linux-gnu/release/asm8051_lsp" "$PSScriptRoot/out/bin/asm8051_lsp" 
+    Copy-Item $path "$PSScriptRoot/out/bin/asm8051_lsp" 
 }
 elseif ($IsWindows && [System.Environment]::Is64BitOperatingSystem) {
-    Copy-Item "$PSScriptRoot/../../../server/asm8051_lsp/target/x86_64-pc-windows-gnu/release/asm8051_lsp.exe" "$PSScriptRoot/out/bin/asm8051_lsp.exe" 
+    $path = "$PSScriptRoot/../../../server/asm8051_lsp/target/x86_64-pc-windows-gnu/release/asm8051_lsp.exe";
+
+    if (!(Test-Path $path)){
+        $path = "$PSScriptRoot/../../../server/asm8051_lsp/target/release/asm8051_lsp.exe";
+    }
+
+    Copy-Item $path "$PSScriptRoot/out/bin/asm8051_lsp.exe" 
+    
 }
 elseif ($IsWindows) {
-    Copy-Item "$PSScriptRoot/../../../server/asm8051_lsp/target/i686-pc-windows-gnu/release/asm8051_lsp.exe" "$PSScriptRoot/out/bin/asm8051_lsp.exe" 
+    $path = "$PSScriptRoot/../../../server/asm8051_lsp/target/i686-pc-windows-gnu/release/asm8051_lsp.exe";
+
+    if (!(Test-Path $path)){
+        $path = "$PSScriptRoot/../../../server/asm8051_lsp/target/release/asm8051_lsp.exe";
+    }
+
+    Copy-Item $path "$PSScriptRoot/out/bin/asm8051_lsp.exe" 
 }
